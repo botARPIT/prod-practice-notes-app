358b117bb53a1d3e3e64f9e70ca04950c56fb02d
botARPIT <arpitchauhanofficial>
2025-12-27 19:27:03 +0530

    test: self made semaphore logic

--- backend/src/notes/note.repo.ts
+++ backend/src/notes/note.repo.ts
@@ -4,29 +4,47 @@ import { withTimeout } from '../utility/backpressure.js'
 import { E_ALREADY_LOCKED, Semaphore, tryAcquire } from 'async-mutex' // Use to apply implicit backpressure, by denying the request upfront
 
 const dbSemaphore = new Semaphore(2)
-
+let allowedLimit = 0
 async function createNote(note: string) {
     try {
         // Used to immediately reject the request if semaphore is unavailable
-        await tryAcquire(dbSemaphore).runExclusive(async () => {
-            const createdNote = await withTimeout(prisma.note.create({
-                data: {
-                    note: note,
-                },
-                select: {
-                    note: true,
-                    // author: true
-                }
-            }), 800
-            )
-            return createdNote
-        })
+        // await tryAcquire(dbSemaphore).runExclusive(async () => {
+        //     const createdNote = await withTimeout(prisma.note.create({
+        //         data: {
+        //             note: note,
+        //         },
+        //         select: {
+        //             note: true,
+        //             // author: true
+        //         }
+        //     }), 800
+        //     )
+        //     return createdNote
+        // })
+        if (allowedLimit > 5) {
+            throw new Error("CONCURRENCY LIMIT HIT, APPLYING BACKPRESSURE")
+        }
+        allowedLimit++;
+        const createdNote = await withTimeout(prisma.note.create({
+            data: {
+                note: note,
+            },
+            select: {
+                note: true,
+                // author: true
+            }
+        }), 800
+        )
+        allowedLimit--;
+        return createdNote
     } catch (error) {
         if (error === E_ALREADY_LOCKED) {
             throw new Error("CONCURRENCY LIMIT HIT, APPLYING BACKPRESSURE")
         } else if (error instanceof Error && error.message === "DB_ACQUIRE_TIMEOUT") {
+            allowedLimit--;
             throw new Error("DB TIMEOUT")
         }
+        allowedLimit--;
         throw new Error("Unexpected Error occured")
     }
 }
